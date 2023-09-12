d265922e0016a515da06b45e6881b40b9bbd9f64
Vytautas Berzvinskas <misionierius@gmail.com>
2023-09-12 21:29:51 +0300

    padaryta pirmos paskaitos praktika

---
+++ Paskaita 1/praktika/index.js
@@ -0,0 +1,6 @@
+console.log("hello");
+import casual from "casual";
+const city = casual.city;
+const random = Math.floor(Math.random() * 10 + 1);
+console.log(city, random);
+console.log("Mr", casual.first_name, casual.last_name);
